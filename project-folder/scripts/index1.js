470fc1f70a7d3a86597fc2176798ac52682263aa
Olena Gert <sarhsian@gmail.com>
2025-03-31 23:24:55 +0200

    Добавлены файл README.md и папка с проектом. / Added README.md file and project folder .

---
+++ project-folder/scripts/index1.js
@@ -0,0 +1,14 @@
+
+const toSecondPage = document.getElementById("toSecondPage")
+
+toSecondPage.addEventListener("click", ()=> {
+window.location.pathname = "/project-folder/html/page2.html"
+})
+
+project-folder/html/page2.html
+
+
+
+
+
+
---
+++ project-folder/scripts/index2.js
@@ -0,0 +1,6 @@
+
+const toMainPage = document.getElementById("toMainPage")
+
+toMainPage.addEventListener("click", ()=> {
+window.location.pathname = "/project-folder/html/index.html"
+})
